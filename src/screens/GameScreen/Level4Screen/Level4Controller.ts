3666c0d371b92ae53d41899d4e4c8e062ec87923
Sonia Chandra <87091116+soniac22@users.noreply.github.com>
2025-11-01 16:45:50 -0700

    setting up level 4 MVC

--- src/screens/GameScreen/Level4Screen/Level4Controller.ts
+++ src/screens/GameScreen/Level4Screen/Level4Controller.ts
@@ -1,3 +1,25 @@
-export class Level4Controller {
-    // to do
+import { Level4Model } from "./Level4Model.ts";
+import { Level4View } from "./Level4View.ts";
+import { ScreenController } from "../../../types.ts";
+import type { ScreenSwitcher } from "../../../types.ts";
+
+export class Level4Controller extends ScreenController {
+    private screenSwitcher: ScreenSwitcher;
+    private model: Level4Model;
+	private view: Level4View;
+
+    constructor(screenSwitcher: ScreenSwitcher) {
+        super();
+        this.screenSwitcher = screenSwitcher;
+
+		this.model = new Level4Model();
+		this.view = new Level4View();
+	}
+
+    /**
+	 * Get the view group
+	 */
+	getView(): Level4View {
+		return this.view;
+	}
 }
