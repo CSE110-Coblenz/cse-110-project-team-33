0b6b18a0eb5e802c0ef7fe40f172bdabb2d60f1c
Sonia Chandra <87091116+soniac22@users.noreply.github.com>
2025-11-01 19:18:24 -0700

    made basic view of level 4

--- src/screens/GameScreen/Level4Screen/Level4View.ts
+++ src/screens/GameScreen/Level4Screen/Level4View.ts
@@ -2,22 +2,34 @@ import Konva from "konva";
 import type { View } from "../../../types.ts";
 import { STAGE_WIDTH, STAGE_HEIGHT } from "../../../constants.ts";
 
-
-export class Level4View  implements View{
+export class Level4View {
+    public layer: Konva.Layer;
     private group: Konva.Group;
+    centerX = STAGE_WIDTH/2;
+    centerY = STAGE_HEIGHT/2;
+    radius = 150;
+
 
     constructor() {
-		this.group = new Konva.Group({ visible: false });
-        
-        // Background
-		const bg = new Konva.Rect({
-			x: 0,
-			y: 0,
-			width: STAGE_WIDTH,
-			height: STAGE_HEIGHT,
-			fill: "#87DEEB", // Sky blue
-		});
-		this.group.add(bg);
+        this.group = new Konva.Group({ visible: false });
+
+        this.layer = new Konva.Layer();
+        this.group = new Konva.Group();
+        this.layer.add(this.group);
+
+        // background
+        const bg = new Konva.Rect({
+            x: 0,
+            y: 0,
+            width: STAGE_WIDTH,
+            height: STAGE_HEIGHT,
+            fill: "#87DEEB",
+        });
+        this.group.add(bg);
+    }
+
+    getLayer(): Konva.Layer {
+        return this.layer;
     }
 
     /**
@@ -37,6 +49,30 @@ export class Level4View  implements View{
     }
 
     getGroup(): Konva.Group {
-		return this.group;
-	}
+        return this.group;
+    }
+
+    drawBase() {
+        const circle = new Konva.Circle({
+            x: this.centerX,
+            y: this.centerY,
+            radius: this.radius,
+            stroke: "black",
+            strokeWidth: 2,
+        });
+
+        const xAxis = new Konva.Line({
+            points: [this.centerX - this.radius - 20, this.centerY, this.centerX + this.radius + 20, this.centerY],
+            stroke: "#aaa",
+        });
+
+        const yAxis = new Konva.Line({
+            points: [this.centerX, this.centerY - this.radius - 20, this.centerX, this.centerY + this.radius + 20],
+            stroke: "#aaa",
+        });
+
+        this.group.add(circle, xAxis, yAxis);
+        this.layer.draw();
+    }
+
 }
